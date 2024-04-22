ce172eaad2bd2a1039e9696a7e2ae18a17bdec9a
Robert Berg <rberg@cps.edu>
2024-04-22 11:05:05 -0500

    finished xor

--- a8.py
+++ a8.py
@@ -1,7 +1,20 @@
-from neural import *
+from neural import NeuralNet
 
-print("<<<<<<<<<<<<<< XOR >>>>>>>>>>>>>>\n")
+# print("<<<<<<<<<<<<<< XOR >>>>>>>>>>>>>>\n")
 
+# xor_data = [
+#     ([0, 0], [0]),
+#     ([1, 0], [1]),
+#     ([0, 1], [1]),
+#     ([0, 0], [0])
+# ]
+
+# xor_nn = NeuralNet(2, 2, 1)
+# xor_nn.train(xor_data, iters=1000, print_interval=100)
+
+# print(xor_nn.test_with_expected(xor_data))
+
+print("<<<<<<<<<<<<<< Voter Data >>>>>>>>>>>>>>\n")
 
 
 
